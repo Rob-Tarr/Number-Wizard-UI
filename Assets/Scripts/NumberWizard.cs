f5793d77d6d174dba267b8f56fb9676f7bff7836
Robert Tarr <robtarr1@gmail.com>
2018-09-20 09:35:54 -0700

    Adding comments to NumberWizard.cs

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -5,10 +5,12 @@ using TMPro;
 
 public class NumberWizard : MonoBehaviour {
 
+    // Serialized field for guess range and guess text
     [SerializeField] int max;
     [SerializeField] int min;
     [SerializeField] TextMeshProUGUI guessText;
 
+    // Int for guess, should be between min and max
     int guess;
     
 
@@ -18,23 +20,28 @@ public class NumberWizard : MonoBehaviour {
         StartGame();
     }
 
+    // initiallizes the first guess of the game
     void StartGame()
     {
         NextGuess();
     }
 
+    // when the guess is higher than the number 
     public void OnPressHigher()
     {
         min = guess + 1;
         NextGuess();
     }
 
+    // when the guess is lower than the number
     public void OnPressLower()
     {
         max = guess - 1;
         NextGuess();
     }
 
+
+    // Function that picks a random number between min and max then prints to string.
     void NextGuess()
     {
         guess = Random.Range(min, max + 1);
