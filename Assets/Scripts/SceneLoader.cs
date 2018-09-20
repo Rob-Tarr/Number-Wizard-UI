9c653e4de48bcea67f31ab468caa5c74e3f1fb4b
Robert Tarr <robtarr1@gmail.com>
2018-09-20 09:38:15 -0700

    Adding Comments to SceneLoader.cs

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// class to handle changing of scenes in game
 public class SceneLoader : MonoBehaviour {
 
+    // method for loading the next scene
     public void LoadNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
 
+    // using SceneManager.LoadScene to load the first scene "0"
     public void LoadStartScene()
     {
     SceneManager.LoadScene(0);
     }
 
+    // Quit option that will stop the application
     public void QuitGame()
     {
         Application.Quit();
